4565bfd148722731a6b87ffedc8b6473528a22bb
iceboy07 <isswarya25@gmail.com>
2019-07-02 15:10:34 +0530

    variable declaration and type

---
+++ variabledeclaration.py
@@ -0,0 +1,6 @@
+a=5 #variable declaration (5 is assigned to variable a)
+print(a,"is a type of", type(a)) #type(a)--> command is used to identify the type of variable 'a' here
+b=7.5
+print(b,"is a type of", type(b))
+c="hello"
+print(c,"is a type of", type(c))
